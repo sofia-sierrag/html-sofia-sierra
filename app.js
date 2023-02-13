4c6ceb03d6c714cd23818a16cebe6e5d93deb284
sofia-sierrag <123768495+sofia-sierrag@users.noreply.github.com>
2023-02-13 12:38:10 -0500

    avance 3

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 document.addEventListener("DOMContentLoaded" , () => {
   const cardAdj = [
-    {
-      name: "",
-      img: "images/"
-    },
-  ]
-}
+    { name: "cab.jpg", img: "images/cab.jpg" },
+    { name: "can.jpg", img: "images/can.jpg" },
+    { name: "del.jpg", img: "images/del.jpg" },
+    { name: "eso.jpg", img: "images/eso.jpg" },
+    { name: "est.jpg", img: "images/est.jpg" },
+    { name: "tib.jpg", img: "images/tib.jpg" }
+  ];
+  
+  const cuadricula = document.querySelector(".cuadricula");
+  const resultado = document.querySelector("#resultado");
+  var cartasescogidas = [];
+  var cartasescogidasid = [];
+  var catasGanadas = [];
